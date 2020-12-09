85f456a0e606fa4be3f33296d1ab156dfda00969
Maria907 <zefir105@yandex.ru>
2020-12-09 18:55:07 +0700

    u

--- Main.cpp
+++ Main.cpp
@@ -4,19 +4,19 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 
 	Payment person1("Olegchenko Oleg Olegovich", 300, 2015, 22);
-    Payment person2("Poshel naxui", 300, 2015, 22);
+    Payment person2;
     person1.calculationSalary();
     person1.calculationPensionContributions();
     person1.calculationIncomeTax();
 
 
-    ofstream save("test.txt", ios_base::app);
+    ofstream save("text.txt", ios_base::app);
     if (save.is_open()) {
         save << person1;
         save.close();
     }
 
-    ifstream load("test.txt", ios_base::in);
+    ifstream load("text.txt", ios_base::in);
     if (load.is_open()) {
         load >> person2;
         load.close();
@@ -27,19 +27,19 @@ int main() {
     person2.calculationIncomeTax();
     cout << person2 << endl;
 
-    ofstream saveB("testBinary.txt", ios_base::binary);
+    ofstream saveB("bText.txt", ios_base::binary);
     person1.binarySave(saveB);
     saveB.close();
 
-    ifstream loadB("testBinary.txt", ios_base::binary);
+    ifstream loadB("bText.txt", ios_base::binary);
     person2.binaryLoad(loadB);
     loadB.close();
     cout << person2 << endl;
 
-    fstream clearFile("test.txt", ios::out);
+    fstream clearFile("text.txt", ios::out);
     clearFile.close();
 
-    fstream clearFileB("testBinary.txt", ios::out);
+    fstream clearFileB("bText.txt", ios::out);
     clearFileB.close();
 
 	allTests();
